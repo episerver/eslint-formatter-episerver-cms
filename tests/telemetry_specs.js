db70a6e84730d8f88e1b03450bd4d607bf3b1190
John-Philip Johansson <john-philip.johansson@episerver.com>
2018-05-03 14:31:34 +0200

    Feature: Add telemetry output and make it default
    Meant to be usable with PowerBI for data collection so we can get a
    better view of what modules are used, and how they are used together.

---
+++ tests/telemetry_specs.js
@@ -0,0 +1,14 @@
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
+const input = require("./fixtures/input.json");
+const expected = require("./fixtures/output-telemetry.json");
+const formatter = require("../lib/telemetry");
+
+describe("telemetry formatter", () => {
+    it("should match expected output", () => {
+        const result = formatter(input);
+        const json = JSON.stringify(expected, null, 4);
+
+        expect(result).to.equal(json);
+    });
+});
